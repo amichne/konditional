8eff1f942820bde7831f319b010d92118ae348b9
amichne <austin@michne.io>
2025-10-31 11:22:27 -0400

    Deployed 6ed570a with MkDocs version: 1.6.1

--- src/main/kotlin/io/amichne/konditional/rules/versions/LeftBound.kt
+++
@@ -1,7 +0,0 @@
-package io.amichne.konditional.rules.versions
-
-import io.amichne.konditional.context.Version
-
-data class LeftBound(
-    override val min: Version,
-) : VersionRange(Type.MIN_BOUND, min, MAX_VERSION)
--- src/main/kotlin/io/amichne/konditional/rules/versions/RightBound.kt
+++
@@ -1,7 +0,0 @@
-package io.amichne.konditional.rules.versions
-
-import io.amichne.konditional.context.Version
-
-data class RightBound(
-    override val max: Version,
-) : VersionRange(Type.MAX_BOUND, MIN_VERSION, max)
