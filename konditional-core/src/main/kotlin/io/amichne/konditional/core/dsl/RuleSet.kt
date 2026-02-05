145b70575f146fa6d1c1602e18bcd974f574a447
amichne <austin@michne.com>
2026-02-04 23:13:50 -0500

    true composition

--- konditional-core/src/main/kotlin/io/amichne/konditional/core/dsl/RuleSet.kt
+++ konditional-core/src/main/kotlin/io/amichne/konditional/core/dsl/RuleSet.kt
@@ -5,6 +5,8 @@ import io.amichne.konditional.core.Namespace
 import io.amichne.konditional.core.features.Feature
 import io.amichne.konditional.internal.builders.RuleBuilder
 import io.amichne.konditional.rules.Rule
+import kotlin.jvm.JvmName
+import kotlin.reflect.KClass
 
 /**
  * A feature-scoped set of rules that can be composed with other rule sets.
@@ -19,7 +21,7 @@ data class RuleSpec<out T : Any, in C : Context>(
 )
 
 @KonditionalDsl
-class RuleSet<RC : Context, T : Any, C, M : Namespace> internal constructor(
+class RuleSet<RC : Context, T : Any, C, M : Namespace> @PublishedApi internal constructor(
     val feature: Feature<T, C, M>,
     internal val rules: List<RuleSpec<T, RC>>,
 ) where C : RC {
@@ -35,7 +37,7 @@ class RuleSet<RC : Context, T : Any, C, M : Namespace> internal constructor(
 }
 
 @KonditionalDsl
-class RuleSetBuilder<T : Any, C : Context> internal constructor() {
+class RuleSetBuilder<T : Any, C : Context> @PublishedApi internal constructor() {
     private val rules = mutableListOf<RuleSpec<T, C>>()
 
     fun rule(
@@ -57,10 +59,24 @@ class RuleSetBuilder<T : Any, C : Context> internal constructor() {
         rules += RuleSpec(value, rule)
     }
 
+    @PublishedApi
     internal fun build(): List<RuleSpec<T, C>> = rules.toList()
 }
 
+@JvmName("ruleSetDefault")
+fun <T : Any, C : Context, M : Namespace> Feature<T, C, M>.ruleSet(
+    build: RuleSetBuilder<T, C>.() -> Unit,
+): RuleSet<C, T, C, M> =
+    RuleSet(feature = this, rules = RuleSetBuilder<T, C>().apply(build).build())
+
+@JvmName("ruleSetWithContextType")
 fun <T : Any, C, M : Namespace, RC : Context> Feature<T, C, M>.ruleSet(
+    @Suppress("UNUSED_PARAMETER") contextType: KClass<RC>,
+    build: RuleSetBuilder<T, RC>.() -> Unit,
+): RuleSet<RC, T, C, M> where C : RC =
+    RuleSet(feature = this, rules = RuleSetBuilder<T, RC>().apply(build).build())
+
+inline fun <reified RC : Context, T : Any, C, M : Namespace> Feature<T, C, M>.ruleSet(
     build: RuleSetBuilder<T, RC>.() -> Unit,
 ): RuleSet<RC, T, C, M> where C : RC =
     RuleSet(feature = this, rules = RuleSetBuilder<T, RC>().apply(build).build())
